3b51aac4b63d4f458ed9e1ed174309a71d62e578
Alaa Abdelfattah <97012156+Alaa-Abdelfattah@users.noreply.github.com>
2022-11-04 23:14:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,522 @@
+//dataaaaaaaaaaa
+hosbitals = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+
+        coordinates: [31.36375007595374, 31.04624821640713],
+      },
+      properties: {
+        phoneFormatted: "(050)2202876",
+        phone: "0502202876",
+        address:
+          "مستشفيات جامعةالمنصورة، عنوان: شارع الجمهورية، ميت خميس وكفر الموجي،مفتوح علي مدار 24 ساعة ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.044115661517374, 31.365037536260207
+        coordinates: [31.365037536260207, 31.044115661517374],
+      },
+      properties: {
+        phoneFormatted: "(050)2944602",
+        phone: "0502944602",
+        address: "مستشفي الدلتا عنوان :جيهان السادات، المنصورة، الدقهلية ",
+
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.044998103818187, 31.364780044198916
+        coordinates: [31.364780044198916, 31.044998103818187],
+      },
+      properties: {
+        phoneFormatted: "(050)2265472",
+        phone: "0502265472",
+        address:
+          " مستشفي طوارئ جامعة المنصورة عنوان: جيهان السادات، ميت خميس وكفر الموجي،",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.050415527567168, 31.39439610850112
+        coordinates: [31.39439610850112, 31.050415527567168],
+      },
+      properties: {
+        phoneFormatted: "(050)2944602",
+        phone: "0502944602",
+        address: "Grand Hospital Mansoura",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.03389344204479, 31.390872573076738
+        coordinates: [31.390872573076738, 31.03389344204479],
+      },
+      properties: {
+        phoneFormatted: "(050) 2307420",
+        phone: "0502307420",
+        address: " مستشفى المنصوره الدولى عنوان: متفرع من ش، عبد السلام عارف",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.045954073745648, 31.36598167381829
+        coordinates: [31.36598167381829, 31.045954073745648],
+      },
+      properties: {
+        address: "المستشفى العام القديم ، عنوان المنصورة، الدقهلية ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.046762960059336, 31.35864437781402
+        coordinates: [31.35864437781402, 31.046762960059336],
+      },
+      properties: {
+        phoneFormatted: "(050) 2230176",
+        phone: "0502230176",
+        address:
+          "مستشفى الباطنة التخصصى جامعة المنصورة عنوان : ميت خميس وكفر الموجي",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.034237647450567, 31.362888641801923
+        coordinates: [31.362888641801923, 31.034237647450567],
+      },
+      properties: {
+        phoneFormatted: "(02)01096360033",
+        phone: "01096360033",
+        address: "مستشفي القمة التخصصي عنوان: عمرو إبن العاص, المنصورة قسم 2",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.03440825250004, 31.362764250794733
+        coordinates: [31.362764250794733, 31.03440825250004],
+      },
+      properties: {
+        phoneFormatted: "(02) 01002003375",
+        phone: "01002003375",
+        address: "Summit Specialized Hospital  أحمد ماهر، المنصورة (قسم 2)",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.034776145204386, 31.394688677814024
+        coordinates: [31.394688677814024, 31.034776145204386],
+      },
+      properties: {
+        phoneFormatted: "(050)2750846",
+        phone: "0502750846",
+        address: "مستشفى الزراعيين عنوان: مجمع المحاكم، المنصورة قسم 2 ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.04132137302658, 31.385418963607428
+        coordinates: [31.385418963607428, 31.04132137302658],
+      },
+      properties: {
+        phoneFormatted: "(050)2359245",
+        phone: "0502359245",
+        address: " مستشفي مجمع الايمان عنوان:سعد الشربيني المنصورة، الدقهلية ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.053601653584842, 31.406361651259363
+        coordinates: [31.406361651259363, 31.053601653584842],
+      },
+      properties: {
+        phoneFormatted: "(050)2334066",
+        phone: "0502334066",
+        address: "مستشفي الخير عنوان : قناة السويس، المنصورة قسم 2 ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.04705727012618, 31.39460284712693
+        coordinates: [31.39460284712693, 31.04705727012618],
+      },
+      properties: {
+        phoneFormatted: "(050) 2332668",
+        phone: "0502332668",
+        address: "مستشفي تبارك للاطفال عنوان :  مأمون الشناوي، المنصورة قسم 2 ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.049336825554416, 31.388165545594575
+        coordinates: [31.388165545594575, 31.049336825554416],
+      },
+      properties: {
+        phoneFormatted: "(050) 2332691",
+        phone: "0502332691",
+        address:
+          "مستشفي الحكمة عنوان :ميدان الشعلة - امام هابى لاند، شارع جامعة الدول العربية",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.037694191688157, 31.379368133724757
+        coordinates: [31.379368133724757, 31.037694191688157],
+      },
+      properties: {
+        address: "  Aomart Al Balabea /مستشفي سما عنوان",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.03713082666138, 31.35798034712693
+        coordinates: [31.35798034712693, 31.03713082666138],
+      },
+      properties: {
+        phoneFormatted: "(050) 0502254884",
+        phone: "0502254884",
+        address: " مستشفي الغزالي عنوان :المنصورة قسم2",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.047768934441095, 31.389925308501123
+        coordinates: [31.389925308501123, 31.047768934441095],
+      },
+      properties: {
+        phoneFormatted: "(050) 2332465",
+        phone: "0502332465",
+        address: "مستشفي مصر الحياة عنوان :مأمون الشناوي، المنصورة قسم 2",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.04453336686539, 31.364519425120076
+        coordinates: [31.364519425120076, 31.04453336686539],
+      },
+      properties: {
+        phoneFormatted: "(050) 2267012",
+        phone: "0502267012",
+        address: "مستشفي الطوارئ عنوان :المنصورة قسم 2 ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.037473564945145, 31.392242737052765
+        coordinates: [31.392242737052765, 31.037473564945145],
+      },
+      properties: {
+        address:
+          "   عنوان:عبد السلام عارف، المنصورة قسم2، اول المنصورة Arab Hospital ",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.047107122748532, 31.365635224052355
+        coordinates: [31.365635224052355, 31.047107122748532],
+      },
+      properties: {
+        phoneFormatted: "(050) 2208557",
+        phone: "0502208557",
+        address: " مستشفى الجزيرة الدولى - السلاب عنوان:اول المنصورة",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.04600409303464, 31.37782318162028
+        coordinates: [31.37782318162028, 31.04600409303464],
+      },
+      properties: {
+        phoneFormatted: "(050) 2219912",
+        phone: "0502219912",
+        address: "مستشفي الشروق العنوان : شارع العباسي بالمنصورة",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.045636413622734, 31.364862747868475
+        coordinates: [31.364862747868475, 31.045636413622734],
+      },
+      properties: {
+        address: "مستشفي جيهان عنوان: جيهان السادات، ميت خميس وكفر الموجي",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        // 31.03394346767059, 31.372930832455694
+        coordinates: [31.372930832455694, 31.03394346767059],
+      },
+      properties: {
+        address: "مستشفي الصدر عنوان:  مستشفى الصدر، المنصورة قسم 2",
+        city: "المنصورة",
+        country: "مصر",
+      },
+    },
+  ],
+};
+
+//get coords
+navigator.geolocation.getCurrentPosition(success, error);
+let coords = [31.378899557661057, 31.033771738768507];
+// // lat
+// :
+// 31.378899557661057
+// lng
+// :
+// 31.033771738768507
+function success(position) {
+  // coords = [position.coords.longitude, position.coords.latitude];
+  renderMap(coords);
+}
+
+function error(position) {
+  console.log(position.message);
+}
+
+// display map
+const accessToken =
+  "pk.eyJ1IjoiYWJkb2VsYXppeiIsImEiOiJja3F3aDBoZ3EwbGo3Mm9xaDRiM2xwd2tqIn0.OQxsy6IpwLSE1F_qdadWvQ";
+
+function renderMap(coords) {
+  mapboxgl.accessToken = accessToken;
+  let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: coords, // starting position
+    zoom: 12,
+  });
+  let directions = new MapboxDirections({ accessToken });
+  directions.setOrigin(coords);
+  map.addControl(new mapboxgl.NavigationControl(), "bottom-left");
+  map.addControl(directions, "top-right");
+  // console.log(map);
+  //assign id
+  hosbitals.features.forEach(function (hosbital, i) {
+    hosbital.properties.id = i;
+  });
+
+  map.on("load", () => {
+    /* Add the data to your map as a layer */
+
+    map.addSource("places", {
+      type: "geojson",
+      data: hosbitals,
+    });
+
+    // flyToStore
+
+    function flyToStore(currentFeature) {
+      map.flyTo({
+        center: currentFeature.geometry.coordinates,
+        zoom: 15,
+      });
+    }
+    // createPopUp
+    function createPopUp(currentFeature) {
+      const popUps = document.getElementsByClassName("mapboxgl-popup");
+      /** Check if there is already a popup on the map and if so, remove it */
+      if (popUps[0]) popUps[0].remove();
+
+      const popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML(
+          `<h3>Hospital</h3><h4>${currentFeature.properties.address}</h4>`
+        )
+        .addTo(map);
+    }
+    map.on("click", (event) => {
+      console.log(event);
+    });
+
+    //Build Location List
+    buildLocationList(hosbitals);
+
+    //Links
+    const links = document.querySelectorAll(".listings .item a");
+
+    //Add event listeners
+    links.forEach((link) => {
+      link.addEventListener("click", function () {
+        for (const feature of hosbitals.features) {
+          if (this.id === `link-${feature.properties.id}`) {
+            flyToStore(feature);
+            createPopUp(feature);
+            directions.setDestination(feature.geometry.coordinates);
+          }
+        }
+        const activeItem = document.getElementsByClassName("active");
+        if (activeItem[0]) {
+          activeItem[0].classList.remove("active");
+        }
+        this.parentNode.classList.add("active");
+      });
+    });
+    //Build Markers And Handle Click on any mark
+    hosbitals.features.map((item) => {
+      const el = document.createElement("div");
+      el.className = "marker marker-image";
+      const marker = new mapboxgl.Marker(el)
+        .setLngLat(item.geometry.coordinates)
+        .addTo(map);
+      hosbitals.features.map((item) => {
+        const el = document.createElement("div");
+        el.className = "marker marker-image";
+        const marker = new mapboxgl.Marker(el)
+          .setLngLat(item.geometry.coordinates)
+          .addTo(map);
+        el.addEventListener("click", (e) => {
+          /* Fly to the point */
+          flyToStore(item);
+          /* Close all other popups and display popup for clicked store */
+          createPopUp(item);
+          directions.setDestination(item.geometry.coordinates);
+          console.log(item.geometry.coordinates);
+          /* Highlight listing in sidebar */
+          const activeItem = document.getElementsByClassName("active");
+          e.stopPropagation();
+          if (activeItem[0]) {
+            activeItem[0].classList.remove("active");
+          }
+          const listing = document.getElementById(
+            `listing-${item.properties.id}`
+          );
+          listing.classList.add("active");
+        });
+      });
+    });
+
+    //USER MARK
+    const userEl = document.createElement("div");
+    userEl.className = "marker marker-user-image";
+    const Usermarker = new mapboxgl.Marker(userEl).setLngLat(coords).addTo(map);
+  });
+}
+
+//Build Store Listing  || helper method
+function buildLocationList(places) {
+  for (const place of places.features) {
+    /* Add a new listing section to the sidebar. */
+    const listings = document.getElementById("listings");
+    const listing = listings.appendChild(document.createElement("div"));
+    /* Assign a unique `id` to the listing. */
+    listing.id = `listing-${place.properties.id}`;
+    /* Assign the `item` class to each listing for styling. */
+    listing.className = "item";
+
+    /* Add the link to the individual listing created above. */
+    const link = listing.appendChild(document.createElement("a"));
+    link.href = "#";
+    link.className = "title";
+    link.id = `link-${place.properties.id}`;
+    link.innerHTML = `${place.properties.address}`;
+
+    /* Add details to the individual listing. */
+    const details = listing.appendChild(document.createElement("div"));
+    details.innerHTML = `${place.properties.city}`;
+    if (place.properties.phone) {
+      details.innerHTML += ` · ${place.properties.phoneFormatted}`;
+    }
+    if (place.properties.distance) {
+      const roundedDistance = Math.round(place.properties.distance * 100) / 100;
+      details.innerHTML += `<div><strong>${roundedDistance} miles away</strong></div>`;
+    }
+  }
+}
+
+window.addEventListener("load", () => {
+  let el = document.querySelector('[for="mapbox-directions-profile-driving"]');
+  if (el) {
+    el.click();
+  }
+});
